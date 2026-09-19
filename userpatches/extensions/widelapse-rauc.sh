43d7016cb1da02f15b255969a081bb763edf25ed
haseebzaib <hzaib76@gmail.com>
2026-09-19 10:40:54 +0500

    rauc image built

--- userpatches/extensions/widelapse-rauc.sh
+++ userpatches/extensions/widelapse-rauc.sh
@@ -5,7 +5,7 @@ function user_config__widelapse_rauc() {
     declare -g ROOTFS_TYPE=ext4 IMAGE_PARTITION_TABLE=msdos
     declare -g OFFSET=8 BOOTSIZE=256 BOOTFS_TYPE=ext4 UEFISIZE=0
     declare -g FIXED_IMAGE_SIZE=7432 USE_HOOK_FOR_PARTITION=yes
-    declare -g EXTRA_IMAGE_SUFFIX="${EXTRA_IMAGE_SUFFIX:-}-rauc-ab"
+    EXTRA_IMAGE_SUFFIXES+=("-rauc-ab")
     add_packages_to_image rauc u-boot-tools libubootenv-tool cloud-guest-utils curl ca-certificates python3 e2fsprogs util-linux
 }
 
@@ -22,7 +22,7 @@ settings = {
  'ENV_MMC_USE_DT':'n', 'ENV_MMC_USE_SW_PARTITION':'n',
  'ENV_OFFSET_RELATIVE_END':'n', 'ENV_OFFSET_REDUND_RELATIVE_END':'n',
  'CMD_SAVEENV':'y', 'CMD_EXT4':'y', 'CMD_PART':'y', 'CMD_SETEXPR':'y',
- 'CMD_FS_GENERIC':'y', 'CMD_BOOTI':'y', 'CMD_SOURCE':'y', 'CMD_TEST':'y',
+ 'CMD_FS_GENERIC':'y', 'CMD_BOOTI':'y', 'CMD_SOURCE':'y', 'HUSH_PARSER':'y',
  'USE_BOOTCOMMAND':'y',
  'BOOTCOMMAND':'"if load mmc 0:1 ${scriptaddr} /boot.scr; then source ${scriptaddr}; fi"',
 }
@@ -42,7 +42,7 @@ function uboot_make_config__widelapse_rauc_validate() {
     for setting in CONFIG_ENV_IS_IN_MMC=y CONFIG_ENV_REDUNDANT=y \
         CONFIG_ENV_OFFSET=0x400000 CONFIG_ENV_OFFSET_REDUND=0x410000 \
         CONFIG_ENV_SIZE=0x10000 CONFIG_ENV_MMC_DEVICE_INDEX=0 \
-        CONFIG_CMD_FS_GENERIC=y CONFIG_CMD_BOOTI=y CONFIG_CMD_SOURCE=y CONFIG_CMD_TEST=y; do
+        CONFIG_CMD_FS_GENERIC=y CONFIG_CMD_BOOTI=y CONFIG_CMD_SOURCE=y CONFIG_HUSH_PARSER=y; do
         grep -qxF "$setting" .config || exit_with_error "U-Boot rejected RAUC setting: $setting"
     done
     grep -q '^CONFIG_BOOTCOMMAND=.*mmc 0:1.*boot.scr' .config || exit_with_error "Missing RAUC boot command"
@@ -67,7 +67,10 @@ PARTITIONS
 }
 
 function format_partitions__widelapse_rauc() {
-    mkfs.ext4 -F -m 0 -L widelapse-data "${LOOP}p4"
+    # Armbian exposes p1/p2 itself; Docker also needs nodes for our extra slots.
+    check_loop_device "${LOOP}p3"
+    check_loop_device "${LOOP}p4"
+    run_host_command_logged mkfs.ext4 -F -m 0 -L widelapse-data "${LOOP}p4"
     # Never use filesystem UUIDs for interchangeable slots.
     cat > "$SDCARD/etc/fstab" <<'FSTAB'
 /dev/root / ext4 defaults,noatime 0 0
